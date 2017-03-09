7e13c2597795e0d7886d2ae146c8b18e0f8be10a
rcristina <ritasofiacristina@gmail.com>
2017-03-09 15:03:26 +0000

    initial commit

---
+++ Game/app/src/main/java/com/example/ritac/game/Comment.java
@@ -0,0 +1,15 @@
+package com.example.ritac.game;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ritacristina on 06/03/17.
+ */
+
+public class Comment extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.comment);
+    }
+}
